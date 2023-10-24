582945ff872f23ab3f6bbf1b3e859be7d1c34bf2
eunicejihyun <eunicejihyun@gmail.com>
2023-10-23 22:47:41 -0400

    day 2 upload

--- index.js
+++ index.js
@@ -1,51 +1,117 @@
-// instantiate empty gameboard positions
-var blockPositions = {
-    "a": [null, null, null, null],
-    "b": [null, null, null, null],
-    "c": [null, null, null, null],
-    "d": [null, null, null, null],
-};
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// instantiate gameboard
+var blockPositions;
+// reset the gameboard
+function resetBoard() {
+    blockPositions = {
+        "a": [null, null, null, null],
+        "b": [null, null, null, null],
+        "c": [null, null, null, null],
+        "d": [null, null, null, null],
+    };
+}
 // get gameboard
 var gameBoard = document.getElementById("gameboard");
 var horizontalPositionOptions = ["a", "b", "c", "d"];
-var verticalPositionOptions = ["1", "2", "3", "4"];
+var verticalPositionOptions = ["0", "1", "2", "3"];
 // setup JS object to look up class corresponding to block value
 var blockClass = {};
 for (var i = 1; i < 13; i++) {
     var value = Math.pow(2, i);
     blockClass[value] = "block-" + String(value);
 }
-console.log(blockClass);
+// check to see if a gamepiece is already in a certain position
 function overlapExists(horizontalPosition, verticalPosition) {
-    console.log(blockPositions[horizontalPosition][Number(verticalPosition) - 1] === null);
-    if (blockPositions[horizontalPosition][Number(verticalPosition) - 1] === null) {
+    if (blockPositions[horizontalPosition][Number(verticalPosition)] === null) {
         return false;
     }
     else {
         return true;
     }
 }
+// recursive function - generate a random location until there is no overlap
 function generateRandomLocation() {
     var randomHorizontal = horizontalPositionOptions[Math.round(Math.random() * 3)];
     var randomVertical = verticalPositionOptions[Math.round(Math.random() * 3)];
     if (overlapExists(randomHorizontal, randomVertical)) {
-        // alert("same location!")
-        generateRandomLocation();
-        return;
+        return '' + generateRandomLocation();
     }
     else {
-        return "position-".concat(randomHorizontal, " position-").concat(randomVertical);
+        return [randomHorizontal, randomVertical];
     }
+}
+// update the gameboard with the new position
+function updateGameBoard(horizontalPosition, verticalPosition, gamePiece) {
+    blockPositions[horizontalPosition][Number(verticalPosition)] = gamePiece;
 }
 function generateNewPiece() {
     var classes = ["gamepiece"];
     var newBlockElement = document.createElement("div");
-    var randomValue = String(Math.round(Math.random() + 1) * 2);
-    newBlockElement.textContent = randomValue;
+    var randomValue = Math.round(Math.random() + 1) * 2;
+    newBlockElement.textContent = String(randomValue);
     classes.push("block-".concat(randomValue));
-    classes.push(generateRandomLocation());
+    var newGameBlock = {
+        "piece": newBlockElement,
+        "value": randomValue,
+    };
+    // get available location
+    var newLocation = generateRandomLocation();
+    var horizontalPosition = newLocation[0];
+    var verticalPosition = newLocation[1];
+    classes.push("position-".concat(horizontalPosition, " position-").concat(verticalPosition));
     newBlockElement.setAttribute("class", classes.join(" "));
+    updateGameBoard(horizontalPosition, verticalPosition, newGameBlock);
     gameBoard ? gameBoard.appendChild(newBlockElement) : alert("no game board");
 }
-generateNewPiece();
-generateNewPiece();
+// start a new game by resetting the board and generating 2 game pieces
+function setupNewGame() {
+    resetBoard();
+    generateNewPiece();
+    generateNewPiece();
+}
+document.onkeydown = function (e) {
+    switch (e.key) {
+        case "ArrowLeft":
+            alert("left");
+            break;
+        case "ArrowUp":
+            shiftVertical(e.key);
+            break;
+        case "ArrowRight":
+            alert("right");
+            break;
+        case "ArrowDown":
+            shiftVertical(e.key);
+            break;
+    }
+};
+function isNotNull(x) {
+    return x !== null;
+}
+function shiftVertical(direction) {
+    horizontalPositionOptions.forEach(function (position) {
+        console.log(position);
+        if (!blockPositions[position].every(function (x) { return x === null; })) {
+            blockPositions[position] = blockPositions[position].filter(isNotNull);
+            var nullsNeeded = 4 - blockPositions[position].length;
+            console.log(blockPositions[position]);
+            for (var i = 0; i < nullsNeeded; i++) {
+                if (direction === "ArrowDown") {
+                    blockPositions[position].push(null);
+                }
+                else {
+                    blockPositions[position].unshift(null);
+                }
+            }
+            console.log(blockPositions[position]);
+            var column = blockPositions[position];
+            column.forEach(function (block, i) {
+                if (block !== null) {
+                    block.piece.setAttribute("class", "gamepiece position-".concat(position, " position-").concat(i, " block-").concat(block.value));
+                }
+            });
+        }
+    });
+}
+setupNewGame();
--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+export {}
+
 interface gameBlock {
     "piece": HTMLElement;
     "value": number;
@@ -12,18 +14,23 @@ interface blockPositionsType {
     "d": positionOptions[];
 }
 
-// instantiate empty gameboard positions
-var blockPositions: blockPositionsType = {
-    "a": [null, null, null, null],
-    "b": [null, null, null, null],
-    "c": [null, null, null, null],
-    "d": [null, null, null, null],
-};
+// instantiate gameboard
+var blockPositions: blockPositionsType;
+
+// reset the gameboard
+function resetBoard() {
+    blockPositions = {
+        "a": [null, null, null, null],
+        "b": [null, null, null, null],
+        "c": [null, null, null, null],
+        "d": [null, null, null, null],
+    };
+}
 
 // get gameboard
 const gameBoard = document.getElementById("gameboard");
 const horizontalPositionOptions = ["a", "b", "c", "d"];
-const verticalPositionOptions = ["1", "2", "3", "4"]
+const verticalPositionOptions = ["0", "1", "2", "3"]
 
 // setup JS object to look up class corresponding to block value
 const blockClass = {}
@@ -31,52 +38,121 @@ for (let i = 1; i < 13; i++) {
     let value = Math.pow(2, i);
     blockClass[value] = "block-" + String(value);
 }
-console.log(blockClass);
 
+// check to see if a gamepiece is already in a certain position
 function overlapExists(horizontalPosition: string, verticalPosition: string) {
-    // console.log(blockPositions[horizontalPosition][Number(verticalPosition)-1] === null)
-    if (blockPositions[horizontalPosition][Number(verticalPosition)-1] === null ) {
+    if (blockPositions[horizontalPosition][Number(verticalPosition)] === null) {
         return false
     } else {
         return true
     }
 }
 
-function generateRandomCoordinates(){
+// recursive function - generate a random location until there is no overlap
+function generateRandomLocation() {
     let randomHorizontal = horizontalPositionOptions[Math.round(Math.random() * 3)];
     let randomVertical = verticalPositionOptions[Math.round(Math.random() * 3)];
-    return [randomHorizontal, randomVertical]
-}
 
-function generateLocation() {
-    let randomLocation = generateRandomCoordinates()
-
-    if (overlapExists(randomLocation[0], randomLocation[1])) {
-        // alert("same location!")
-        generateRandomLocation()
+    if (overlapExists(randomHorizontal, randomVertical)) {
+        return '' + generateRandomLocation()
     } else {
-        return `position-${randomHorizontal} position-${randomVertical}`;
+        return [randomHorizontal, randomVertical];
     }
 }
 
-function updateGameBoard(blockPositions: blockPositionsType) {
-
+// update the gameboard with the new position
+function updateGameBoard(horizontalPosition: string, verticalPosition: string, gamePiece: gameBlock) {
+    blockPositions[horizontalPosition][Number(verticalPosition)] = gamePiece
 }
 
 
 function generateNewPiece() {
     let classes = ["gamepiece"];
     var newBlockElement = document.createElement("div");
-    let randomValue = String(Math.round(Math.random() + 1) * 2);
-    newBlockElement.textContent = randomValue;
+    let randomValue = Math.round(Math.random() + 1) * 2;
+    newBlockElement.textContent = String(randomValue);
     classes.push(`block-${randomValue}`)
-    classes.push(generateRandomLocation());
+
+    let newGameBlock: gameBlock = {
+        "piece": newBlockElement,
+        "value": randomValue,
+    }
+
+    // get available location
+    let newLocation = generateRandomLocation()
+    let horizontalPosition = newLocation[0]
+    let verticalPosition = newLocation[1]
+    classes.push(`position-${horizontalPosition} position-${verticalPosition}`);
     newBlockElement.setAttribute("class", classes.join(" "))
+
+    updateGameBoard(horizontalPosition, verticalPosition, newGameBlock)
 
     gameBoard ? gameBoard.appendChild(newBlockElement) : alert("no game board")
 }
 
+// start a new game by resetting the board and generating 2 game pieces
+function setupNewGame() {
+    resetBoard()
+    generateNewPiece()
+    generateNewPiece()
+}
 
-generateNewPiece()
+document.onkeydown = function (e) {
+    switch (e.key) {
+        case "ArrowLeft":
+            alert("left")
+            break;
+        case "ArrowUp":
+            shiftVertical(e.key)
+            break;
+        case "ArrowRight":
+            alert("right")
+            break;
+        case "ArrowDown":
+            shiftVertical(e.key)
+            break;
+    }
+};
 
-generateNewPiece()
+function isNotNull(x: positionOptions) {
+    return x !== null;
+}
+
+
+function shiftVertical(direction: "ArrowDown" | "ArrowUp") {
+
+    horizontalPositionOptions.forEach((position: string) => {
+        console.log(position)
+
+        if (!blockPositions[position].every((x: positionOptions) => x === null)) {
+
+            
+
+            blockPositions[position] = blockPositions[position].filter(isNotNull)
+            let nullsNeeded = 4 - blockPositions[position].length
+            console.log(blockPositions[position])
+            for (let i = 0; i < nullsNeeded; i++) {
+                if (direction === "ArrowDown") {
+                    blockPositions[position].push(null)
+                } else {
+                    blockPositions[position].unshift(null)
+                }
+            }
+
+            console.log(blockPositions[position])
+
+            let column = blockPositions[position]
+
+            column.forEach((block: positionOptions, i: number) => {
+                if (block !== null) {
+                    block.piece.setAttribute("class", `gamepiece position-${position} position-${i} block-${block.value}`)
+                }
+            })
+        }
+
+
+    })
+}
+
+
+setupNewGame()
